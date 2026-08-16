96ca7129ef6c1e043c74921130ac8ea67b555be3
Zahra Hassani <hassanizahra897@gmail.com>
2026-08-16 05:41:10 +0430

    Add If.dart with basic age check and print statements

---
+++ If.dart
@@ -0,0 +1,9 @@
+void main() {
+  String myName = "Ali";
+  int age = 22;
+  print(myName);
+  print(age);
+  if(age >= 18){
+  print("Hi there, my name is $myName and I'm $age years old");
+  }
+  }
