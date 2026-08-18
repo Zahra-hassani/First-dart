7c3add0513a6caf92f9f1b51fa69dbd3637075a2
Zahra Hassani <hassanizahra897@gmail.com>
2026-08-19 01:24:13 +0430

    Add list functions demonstration in ListFunctions.dart
    Implement list functions in Dart to demonstrate various operations on a list of integers.

---
+++ ListFunctions.dart
@@ -0,0 +1,18 @@
+void main() {
+  /* list functions */
+  List<int> numbers = [1,45,38,45,89,67,87,63,98];
+  
+  print(numbers.length);
+  print(numbers.first);
+  print(numbers.last);
+  print(numbers.isEmpty);
+  print(numbers.reversed);
+  
+  numbers.add(99);
+  numbers.insert(1,2);
+  print(numbers.removeLast());
+  numbers.removeWhere((i) => 70 > 90);
+  print(numbers.contains(99));
+  print(numbers.indexOf(89));
+  print(numbers.indexWhere((i) => i % 8 == 0));
+}
