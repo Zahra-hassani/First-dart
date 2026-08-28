3598de0cefceafc54894314c7cead94d161d41fc
Zahra-developer <hassanizahra897@gmail.com>
2026-08-28 21:59:17 +0430

    Inheirtance for class

---
+++ inheirtance.dart
@@ -0,0 +1,26 @@
+abstract class Employee{
+  String name;
+  String lastName;
+  String gender;
+  String phoneNumber;
+  String department;
+  Employee(this.name,this.lastName,this.department,this.phoneNumber,this.gender);
+
+  void work(){
+    print("${this.name} works in ${this.department} department.");
+  }
+}
+
+// child class
+class softwareEngineer extends Employee {
+  String field;
+  softwareEngineer(String name, String lastName, String department, String phoneNumber, String gender, this.field):super (name, lastName, department, phoneNumber, gender);
+
+}
+
+void main(List<String> args) {
+    softwareEngineer Sara = softwareEngineer("Sara", "Moradi", "Developement", "0792323146", "Female", "Computer Science");
+    print("${Sara.name} ${Sara.lastName} is our new Employee.");
+    Sara.work();
+}
+
