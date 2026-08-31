3716f49863b69170999e19045a1dde6616063797
Zahra-developer <hassanizahra897@gmail.com>
2026-08-31 15:23:48 +0430

    inheirtance practicing

--- inheirtance.dart
+++ inheirtance.dart
@@ -18,9 +18,36 @@ class softwareEngineer extends Employee {
 
 }
 
+// parent class
+class Animal {
+  String name;
+  String color;
+  String size;
+  int age;
+  Animal(this.name, this.color, this.size, this.age);
+
+  void eat(){
+    print("${this.name} can eat something.");
+  }
+}
+
+// child class
+class Cat extends Animal{
+  num height;
+  num weight;
+  Cat(String name, String color, String size, int age, this.height, this.weight):super (name, color, size, age);
+
+}
+
 void main(List<String> args) {
     softwareEngineer Sara = softwareEngineer("Sara", "Moradi", "Developement", "0792323146", "Female", "Computer Science");
     print("${Sara.name} ${Sara.lastName} is our new employee.");
     Sara.work();
+
+    // cat's sample
+    Cat ketty = Cat("Ketty", "gray","small", 2, 20, 2);
+    ketty.weight = 1.4;
+    print("I've a kitten by the name of ${ketty.name}. it has ${ketty.height} centi meters height, and ${ketty.weight} kilo gram weight.");
+    ketty.eat();
 }
 
