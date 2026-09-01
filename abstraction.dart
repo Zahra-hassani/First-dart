e68203a0fe7855fc0814433b4cb31c98119c944f
Zahra-developer <hassanizahra897@gmail.com>
2026-09-01 11:35:44 +0430

    abstraction practicing

--- abstraction.dart
+++ abstraction.dart
@@ -45,9 +45,47 @@ class Pasta extends Food {
   }
 }
 
+// abstraction practice
+
+class Electronics{
+  String name;
+  Electronics(this.name);
+
+  void usage(){}
+}
+
+// child class
+class Mobile extends Electronics{
+  int memory;
+  int RAM;
+  bool hasCamera;
+  int price;
+  Mobile(String name, this.memory, this.RAM, this.hasCamera, this.price):super (name);
+
+  void usage(){
+    print("${this.name} is an Electronic Device that is able to save some files; like: pictures, videos, music and things.");
+  }
+}
+
+class Headphones extends Electronics{
+  bool isWireless;
+  int price;
+  Headphones(String name, this.isWireless, this.price):super (name);
+
+  void usage(){
+    print("${this.name} is an Electronic Device that is able to play music.");
+  }
+}
+
 void main(List<String> args) {
   Pasta pasta1 = Pasta("New Indian Pasta",200,"A delecious and spicy food for spicy-lovers.");
   pasta1.ingredients();
   pasta1.receipe();
+
+  Mobile iPhoneX = Mobile("iPhone X", 64, 8, true, 9000);
+  iPhoneX.usage();
+
+  Headphones samsungHeadphones = Headphones("Samsung Headphones", true, 500);
+  samsungHeadphones.usage();
 }
 
