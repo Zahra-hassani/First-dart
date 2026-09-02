3c91a813fbd0cfcdd32289da1c125bd7c42c564a
Zahra-developer <hassanizahra897@gmail.com>
2026-09-02 11:03:45 +0430

    new example for class

--- class.dart
+++ class.dart
@@ -5,6 +5,17 @@ class Animal {
   num weight;
   Animal(this.name,this.size,this.height,this.weight);
 }
+
+class Product{
+  int id;
+  String name;
+  String brand;
+  String category;
+  num totalPrice;
+  int amount;
+  Product(this.id, this.name, this.brand, this.category, this.totalPrice, this.amount);
+}
+
 void main() {
   var cat = new Animal("Cat", "Small", 42, 3.9);
   print("Our first instance is ${cat.name}. it is a ${cat.size} animal with almost ${cat.height}. it is really cute.");
@@ -13,4 +24,8 @@ void main() {
   var sheep = new Animal("Sheep", "Big", 69, 28.60);
   print(sheep.name);
   print("The next instance of animals is ${sheep.name}. It is a ${sheep.size} animal with almost ${sheep.height}.");
+
+
+  Product faceWash = Product(1, "Donkey milk Facewash", "Donkey Milk","cosmic items", 200000, 3800);
+  print(faceWash);
 }
