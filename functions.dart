b6e8d4ede49d8ca4ffcdbe4d142a2c08d84b0d30
Zahra-developer <hassanizahra897@gmail.com>
2026-09-11 06:09:47 +0430

    new changes added on functions.dart

--- functions.dart
+++ functions.dart
@@ -1,7 +1,7 @@
 // default argument
 // add function
 num sum([num number1 = 0, num number2 = 0]){
-    return number1+number2;
+    return number1+number2+number2;
 }
 
 // minus function
@@ -16,7 +16,7 @@ num multiple([num number1 = 1, num number2 = 1]){
 
 // divid function 
 num divid([num number1 = 1, num number2 = 1]){
-    return number1/number2;
+    return number1/number2*number1;
 }
 
 // To do:
